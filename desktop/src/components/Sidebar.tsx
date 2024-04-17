4caefe43c97deb3912d0b5c94851373a38f85bd7
Christian Caliendo <90165610+ChrisCaliendo@users.noreply.github.com>
2024-04-16 23:22:24 -0400

    added correct icons

--- desktop/src/components/Sidebar.tsx
+++ desktop/src/components/Sidebar.tsx
@@ -1,13 +1,30 @@
+import {  IconBooks,  IconHistory,  IconRobot, IconSearch, IconSettings } from "@tabler/icons-react";
 import SidebarButton from "./SidebarButton";
 
 const Sidebar = () => {
     return (  
-        <div className='grid grid-cols-1'>
-            <SidebarButton />
-            <SidebarButton />
-            <SidebarButton />
-            <SidebarButton />
-            <SidebarButton />
+        <div className='grid grid-cols-1 mt-2'>
+            
+            <SidebarButton 
+                icon={<IconBooks color="white" size={42} />} 
+                routeName="hello world"
+            />
+            <SidebarButton 
+                icon={<IconSearch color="white" size={42} />} 
+                routeName="hello world"
+            />
+            <SidebarButton 
+                icon={<IconSettings color="white" size={42} />} 
+                routeName="hello world"
+            />
+            <SidebarButton 
+                icon={<IconHistory color="white" size={42} />} 
+                routeName="hello world"
+            />
+            <SidebarButton 
+                icon={<IconRobot color="white" size={42} />} 
+                routeName="hello world"
+            />
         </div>
         
     );
--- desktop/src/components/SidebarButton.tsx
+++ desktop/src/components/SidebarButton.tsx
@@ -1,14 +1,15 @@
 import { IconRobot } from "@tabler/icons-react";
 import React from "react";
 
-interface Props {
-    Icon: React.ElementType
+interface SidebarButtonProps {
+    icon?: React.ReactNode;
+    routeName?: string;
 }
 
-const SidebarButton = () => {
+const SidebarButton = ( { icon, routeName }: SidebarButtonProps) => {
     return (  
-        <div className='transition ease-in-out duration-300 text-3xl text-white font-sans bg-slate-600 bg w-14 h-14 hover:scale-110 my-1 mx-2 rounded-full flex justify-center items-center'>
-            <IconRobot color="white" size={42} />
+        <div className='transition ease-in-out duration-300 font-thin text-white bg-slate-600 bg w-14 h-14 hover:scale-110 my-1 mx-2 rounded-full flex justify-center items-center'>
+            {icon}
         </div>
         
     );
