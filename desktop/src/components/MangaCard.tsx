00b47c64321ab70823e655eb4fd6ebe146f83ae1
Christian Caliendo <90165610+ChrisCaliendo@users.noreply.github.com>
2024-04-17 14:44:48 -0400

    added library buttons

--- desktop/src/components/MangaCard.tsx
+++ desktop/src/components/MangaCard.tsx
@@ -13,8 +13,9 @@ const MangaCard = ({ mangaId, title, description, image }: MangaCardProps) => {
             <Link to={`/manga/${mangaId}`}>
                 <img className="rounded-2xl" src={image} alt="" />
                 <div className="h-full w-full rounded-2xl absolute bottom-0 z-10 bg-slate-500 bg-opacity-35 transition ease-in-out duration-300 opacity-0 hover:opacity-100">
-                    <p className="py-1 px-2 font-bold text-white">{description}</p>
-                    <p className="py-1 px-2 font-bold text-white bottom-0">{title}</p>
+                <p className="py-1 px-2 font-bold text-white h-1/6">{title}</p>
+                    <p className="py-1 px-2 font-bold text-white h-5/6">{description}</p>
+                    
                 </div>
             </Link>
             
--- desktop/src/pages/LibraryPage.tsx
+++ desktop/src/pages/LibraryPage.tsx
@@ -1,9 +1,18 @@
+import { IconMoon } from "@tabler/icons-react";
 import MangaCard from "../components/MangaCard";
 
 const LibraryPage = () => {
     return ( 
         <div className='h-screen bg-gray-100 px-5 w-full'>
-            <h1 className="text-3xl p-4 font-bold">Library</h1>
+            <div className="w-full flex ">
+                <h1 className="text-3xl p-4 font-bold">Library</h1>
+                <button className="font-semibold px-2 py-1 m-3  bg-slate-300 rounded-xl border-2 active:bg-slate-200">Layout</button>
+                <button className="font-semibold px-2 py-1 m-3  bg-slate-300 rounded-xl border-2 active:bg-slate-200"><IconMoon size={20}/></button>
+                <input className="font-semibold w-1/3 h-8 mt-4 mx-2 p-3 py-4 border-slate-700 rounded-xl border-2 align-middle" placeholder="Search"/>
+                
+                
+            </div>
+            
             <div className="grid grid-col-4 grid-row-3 grid-flow-col gap-3">
                 
                 <MangaCard 
