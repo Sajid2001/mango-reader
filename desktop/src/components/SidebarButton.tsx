4735fc25e6c2546248d073aef57aabdb3389d080
Christian Caliendo <90165610+ChrisCaliendo@users.noreply.github.com>
2024-04-17 23:09:54 -0400

    library looking good

--- desktop/src/components/SidebarButton.tsx
+++ desktop/src/components/SidebarButton.tsx
@@ -10,7 +10,7 @@ interface SidebarButtonProps {
 const SidebarButton = ( { icon, routeName, pageName }: SidebarButtonProps) => {
     return (  
         <Link to={routeName} className="rounded-full" title={pageName} data-placement="right" data-te-toggle="tooltip">
-            <div className='transition ease-in-out duration-300 font-thin text-white bg-slate-600 bg w-14 h-14 hover:scale-110 my-1 mx-2 rounded-full flex justify-center items-center'>
+            <div className='transition ease-in-out duration-300 font-thin text-white bg-slate-600 bg w-14 h-14 hover:scale-110 my-1 ml-2 mr-3 rounded-full flex justify-center items-center'>
 
                 {icon}
             
--- desktop/src/pages/LibraryPage.tsx
+++ desktop/src/pages/LibraryPage.tsx
@@ -6,14 +6,14 @@ const LibraryPage = () => {
         <div className='h-screen bg-gray-100 px-5 w-full'>
             <div className="w-full flex ">
                 <h1 className="text-3xl p-4 font-bold">Library</h1>
-                <button className="font-semibold px-2 m-3  bg-slate-300 rounded-lg border-2 active:bg-slate-200">Layout</button>
-                <button className="font-semibold px-2 m-3  bg-slate-300 rounded-lg border-2 active:bg-slate-200"><IconMoon size={20}/></button>
-                <input className="font-semibold w-1/3 h-8 mt-3 mx-2 p-3 py-5 mb-1  rounded-lg border-2 align-middle bg-slate-300 placeholder:text-black" placeholder="Search Library..."/>
+                <button className="font-semibold text-lg px-3 m-3  bg-slate-300 rounded-lg border-2 active:bg-slate-200">Layout</button>
+                <button className="font-semibold px-3 m-3  bg-slate-300 rounded-lg border-2 active:bg-slate-200"><IconMoon size={24}/></button>
+                <input className="font-semibold w-1/3 h-8 mt-3 mx-2 p-3 py-5 mb-1  text-lg rounded-lg border-2 align-middle bg-slate-300 placeholder:text-black" placeholder="Search Library..."/>
                 
                 
             </div>
             
-            <div className="grid grid-row-3 grid-col-6 grid-flow-col gap-4">
+            <div className="grid grids-col-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-4">
                 
                 <MangaCard 
                     title="Manga Title"
