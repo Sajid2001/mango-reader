e0ec39d8287d36b8180371b4488e80b8dedf5c8f
Christian Caliendo <90165610+ChrisCaliendo@users.noreply.github.com>
2024-05-10 12:17:12 -0400

    Update main.ts

--- desktop/electron/main.ts
+++ desktop/electron/main.ts
@@ -51,6 +51,11 @@ ipcMain.on("saveData", (sender, data) => {
   fs.writeDataToFile("library.json", sData);
 });
 
+ipcMain.on("readData", (sender, data) => {
+  let sData = JSON.stringify(data);
+  fs.writeDataToFile("library.json", sData);
+});
+
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
