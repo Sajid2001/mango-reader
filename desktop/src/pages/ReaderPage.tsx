de244985dd571226872217942c3b821f8c797172
Christian Caliendo <90165610+ChrisCaliendo@users.noreply.github.com>
2024-05-29 17:42:51 -0400

    working on Reader Functionality

--- desktop/src/pages/ReaderPage.tsx
+++ desktop/src/pages/ReaderPage.tsx
@@ -1,5 +1,5 @@
-import { IconArrowBackUp, IconArrowsMaximize, IconBook, IconChevronLeft,  IconChevronRight,  IconCircleArrowRight,  IconKeyboard,  IconSettings,  IconX } from "@tabler/icons-react";
-import { useEffect, useState } from "react";
+import { IconArrowBackUp, IconArrowsMaximize, IconArrowsMinimize, IconBook, IconChevronLeft,  IconChevronRight,  IconCircleArrowLeft,  IconCircleArrowRight,  IconKeyboard,  IconPageBreak,  IconSettings,  IconX } from "@tabler/icons-react";
+import { useEffect, useRef, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 
 const ReaderPage = () => {
@@ -13,6 +13,11 @@ const ReaderPage = () => {
     const [maxChapters, setMaxChapters] = useState<number>(0);
     const [chapterName, setChapterName] = useState<string>("test");
 
+    const [singlePage, setSinglePage] = useState<boolean>(false);
+    const [fitHeight, setFitHeight] = useState<boolean>(false);
+    const [leftToRight, setLeftToRight] = useState<boolean>(false);
+
+    const imageRefs = useRef([]);
 
     useEffect(() => {
         fetch("http://127.0.0.1:8000/api/manga/"+Number(mangaId))
@@ -31,29 +36,53 @@ const ReaderPage = () => {
 
     useEffect(() => {
         fetch("http://127.0.0.1:8000/api/chapters/"+Number(mangaId))
-                .then(response => {
-                    if (!response.ok) {
-                    throw new Error('Network response was not ok');
-                }
-                return response.json();
-                }).then(data => {
-                    // Map fetched data to Post model
-                    setChapterName(data[Number(chapterId)].chapter_name)
-                })
+            .then(response => {
+                if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+            }).then(data => {
+                // Map fetched data to Post model
+                setChapterName(data[Number(chapterId)-1].chapter_name)
+            })
                 .catch(error => console.error('Error fetching chapter data:', error));
         fetch("http://127.0.0.1:8000/api/chapters/"+Number(mangaId)+"/"+Number(chapterId))
-                .then(response => {
-                    if (!response.ok) {
-                    throw new Error('Network response was not ok');
+            .then(response => {
+                if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+            }).then(data => {
+                // Map fetched data to Post model
+                const mappedData = data.map((chapter: any) => chapter.scan_url);
+                
+                setScans(mappedData);
+            })
+            .catch(error => console.error('Error fetching chapter data:', error));
+        const observer = new IntersectionObserver(
+            (entries) => {
+                entries.forEach(entry => {
+                if (entry.isIntersecting) {
+                    setCurrentPage(Number(entry.target.id));
                 }
-                return response.json();
-                }).then(data => {
-                    // Map fetched data to Post model
-                    const mappedData = data.map((chapter: any) => chapter.scan_url);
-                    
-                    setScans(mappedData);
-                })
-                .catch(error => console.error('Error fetching chapter data:', error));
+                });
+            },
+            {
+                root: null,
+                rootMargin: '0px',
+                threshold: 0.5,
+            }
+            );
+        
+            imageRefs.current.forEach((image: any) => {
+                if (image) observer.observe(image);
+            });
+        
+            return () => {
+            imageRefs.current.forEach((image: any) => {
+                if (image) observer.unobserve(image);
+            });
+        };
     }, [mangaId, chapterId]);
 
     return ( 
@@ -68,22 +97,22 @@ const ReaderPage = () => {
                         </div>
                         <div className="flex flex-col w-full font-semibold px-5 text-lg *:w-full *:flex *:px-2 *:py-1 *:items-center *:justify-between  *:bg-slate-100 *:rounded-lg *:my-2">
                             <div className="*:rounded-lg">
-                                <Link to={`/reader/${mangaId}/${Math.max(Number(chapterId)-1, 1)}`} className="hover:bg-slate-200 active:bg-slate-400"><IconChevronLeft size={IconSize}/></Link>
-                                {chapterName}
-                                <Link to={`/reader/${mangaId}/${Math.min(Number(chapterId)+1, maxChapters)}`}  className="hover:bg-slate-200 active:bg-slate-400 disabled:bg-green-500"><IconChevronRight size={IconSize}/></Link>
+                                <Link to={`/reader/${mangaId}/${Math.max(Number(chapterId)-1, 1)}`} className={`${Number(chapterId) == 0 ? "pointer-events-none" : "hover:bg-slate-200 active:bg-slate-400"}`}><IconChevronLeft size={IconSize} color={`${Number(chapterId) != 0 ? "black" : "inherit"}`}/></Link>
+                                    <p className="text-center">{chapterName}</p>
+                                <Link to={`/reader/${mangaId}/${Math.min(Number(chapterId)+1, maxChapters)}`}  className={`${Number(chapterId) == maxChapters ? "pointer-events-none" : "hover:bg-slate-200 active:bg-slate-400"}`}><IconChevronRight size={IconSize} color={`${Number(chapterId) != maxChapters ? "black" : "inherit"}`}/></Link>
                             </div>
                             <div className="*:rounded-lg">
-                                <button className="hover:bg-slate-200 active:bg-slate-400"><IconChevronLeft size={IconSize}/></button>
+                                <button onClick={() => ""} className="hover:bg-slate-200 active:bg-slate-400"><IconChevronLeft size={IconSize}/></button>
                                 1/{scans?.length}
                                 <button className="hover:bg-slate-200 active:bg-slate-400"><IconChevronRight size={IconSize}/></button>
                             </div>
                         </div>
-                        <div className="flex flex-col w-full font-semibold px-5 text-lg *:w-full *:flex *:px-2 *:py-1 *:items-center *:justify-between  *:bg-slate-100 *:rounded-lg *:my-2">
-                            <button className="hover:bg-slate-200 active:bg-slate-400">Single Page <IconBook size={IconSize}/></button>
-                            <button className="hover:bg-slate-200 active:bg-slate-400">Fit Height <IconArrowsMaximize size={IconSize}/></button>
-                            <button className="hover:bg-slate-200 active:bg-slate-400">Left To Right <IconCircleArrowRight size={IconSize}/></button>
-                            <button className="hover:bg-slate-200 active:bg-slate-400">Keybinds <IconKeyboard size={IconSize}/></button>
-                            <button className="hover:bg-slate-200 active:bg-slate-400">Settings <IconSettings size={IconSize}/></button>
+                        <div className="flex flex-col w-full font-semibold px-5 text-lg  *:*::w-full *:*:flex *:px-2 *:py-1 *:*:items-center *:*:justify-between  *:bg-slate-100 *:rounded-lg *:my-2">
+                            <button onClick={() => setSinglePage(!singlePage)} className="hover:bg-slate-200 active:bg-slate-400">{ singlePage ? <div>Longstrip <IconPageBreak size={IconSize}/></div> : <div>Single Page <IconBook size={IconSize}/></div>}</button>
+                            <button onClick={() => setFitHeight(!fitHeight)} className="hover:bg-slate-200 active:bg-slate-400">{ fitHeight ?  <div>Fit Width <IconArrowsMinimize size={IconSize}/></div> : <div>Fit Height <IconArrowsMaximize size={IconSize}/></div>}</button>
+                            {singlePage && <button onClick={() => setLeftToRight(!leftToRight)} className="hover:bg-slate-200 active:bg-slate-400">{ leftToRight ? <div>Right to Left <IconCircleArrowLeft size={IconSize}/></div> : <div>Left to Right <IconCircleArrowRight size={IconSize}/></div>}</button>}
+                            <button className="hover:bg-slate-200 active:bg-slate-400"><div>Keybinds <IconKeyboard size={IconSize}/></div></button>
+                            <button className="hover:bg-slate-200 active:bg-slate-400"><div>Settings <IconSettings size={IconSize}/></div></button>
                         </div>
                     </div>
                     
@@ -96,7 +125,7 @@ const ReaderPage = () => {
                 ?
                 <div className="grid grid-cols-1">
                     {
-                        scans.map((scan: string, index: number) => <img src={scan} id={`scan-${index}`}  className="flex justify-self-center"/>)
+                        scans.map((scan: string, index: number) => <img src={scan} id={`${index}`}  className="flex justify-self-center"/>)
                     }
                 </div>
                 :
