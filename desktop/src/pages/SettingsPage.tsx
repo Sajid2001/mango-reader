9b91b94356273dae07b255dc39493621ac20566b
Christian Caliendo <90165610+ChrisCaliendo@users.noreply.github.com>
2024-06-14 00:02:28 -0400

    blah

--- desktop/src/pages/SettingsPage.tsx
+++ desktop/src/pages/SettingsPage.tsx
@@ -9,7 +9,7 @@ const SettingsPage = () => {
     const navigate = useNavigate();
 
     return (  
-        <div className="w-full flex-col">
+        <div className="h-screen w-full flex-col overflow-y-auto">
             <div className="flex">
                 <h1 className="text-3xl p-4 font-bold">Settings</h1>
                 <button className="font-semibold text-lg px-4 m-5 text-white  bg-slate-900 rounded-lg active:bg-slate-700">Reset</button>
@@ -20,7 +20,9 @@ const SettingsPage = () => {
                 <button onClick={() => navigate("/settings/general")} disabled={location.pathname === "/settings/general"} className="transition ease-in-out disabled:border-b-4 disabled:border-black text-2xl p-4 border-1 border-slate-600">General</button>
                 <button onClick={() => navigate("/settings/reader")}  disabled={location.pathname === "/settings/reader"}  className="transition ease-in-out disabled:border-b-4 disabled:border-black text-2xl p-4 border-1 border-slate-300">Reader</button>
             </div>
+            <div className="">
             <Outlet/>
+            </div>
         
             
             
