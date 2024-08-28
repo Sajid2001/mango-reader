1d42935ceffac8efbb2dd03ad13912e71465f207
Christian Caliendo <90165610+ChrisCaliendo@users.noreply.github.com>
2024-08-28 15:18:48 -0400

    Update ChapterItem.tsx

--- desktop/src/components/ChapterItem.tsx
+++ desktop/src/components/ChapterItem.tsx
@@ -11,22 +11,24 @@ interface Props {
 }
 
 const ChapterItem = ({ mangaId, chapter, reading }: Props) => (
-  <Link
-    to={`/reader/${mangaId}/${chapter.chapterNumber}`}
-    className={`flex justify-between p-3 items-center font-bold hover:bg-secondary hover:opacity-80 ${
-      reading && reading.progress >= chapter.chapterNumber && "font-light"
-    }`}
-  >
-    <div className="flex-col">
-      <p className="text-md">{chapter.chapterName}</p>
-      <p>{chapter.chapterNumber}</p>
-    </div>
-    <div>
-      <button className="bg-primary border-2 border-background rounded-lg text-text py-1 px-3 mr-4 hover:bg-background active:bg-accent">
+  <div className="w-full flex">
+    <Link
+      to={`/reader/${mangaId}/${chapter.chapterNumber}`}
+      className={`flex justify-between p-3 items-center font-bold hover:bg-secondary hover:opacity-80 w-full ${
+        reading && reading.progress >= chapter.chapterNumber && "font-light"
+      }`}
+    >
+      <div className="flex-col">
+        <p className="text-md">{chapter.chapterName}</p>
+        <p>{chapter.chapterNumber}</p>
+      </div>
+    </Link>
+    <div className="flex bg-accent p-2 items-center justify-center">
+      <button className="bg-primary border-2 border-background rounded-lg text-text py-1 px-3 mx-4 hover:bg-background active:bg-accent">
         <IconDownload />
       </button>
     </div>
-  </Link>
+  </div>
 );
 
 export default React.memo(ChapterItem);
